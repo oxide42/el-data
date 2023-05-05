258ba72bb2220441b6678e6e6c7de6918c42d4ad
oxide42 <oxide42@timeline.dk>
2023-05-05 14:48:36 +0200

    Project setup

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template, Blueprint, request
+from flask import Flask
 import core.service
 
 app = Flask(__name__)
-
 core.service.init_app()
+
+
+@app.route("/api/refresh_data")
+def refresh_data():
+    core.service.refresh_data()
+    return {}
+
 
 if __name__ == '__main__':
     app.run()
